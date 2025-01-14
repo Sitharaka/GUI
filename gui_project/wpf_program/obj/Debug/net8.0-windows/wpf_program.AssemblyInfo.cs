bc338cc0442839acf6ea6455d46625e2899659dc
Sitharaka <sitharaka.jayawardhana@gmail.com>
2025-01-14 22:52:00 +0530

    Commit check

--- gui_project/wpf_program/obj/Debug/net8.0-windows/wpf_program.AssemblyInfo.cs
+++ gui_project/wpf_program/obj/Debug/net8.0-windows/wpf_program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("wpf_program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459afdac9b761b73abbba848afc43aca1bf8a0ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4149ea916e8dc42bb96d7d4826f3721707f5909")]
 [assembly: System.Reflection.AssemblyProductAttribute("wpf_program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wpf_program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
